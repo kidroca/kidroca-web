5e96a3e95e463a6648d451979435526fd805bb38
Peter Velkov <kidroca@gmail.com>
2016-07-03 23:22:49 +0300

    Updated navigation links

--- routes/middleware.js
+++ routes/middleware.js
@@ -19,9 +19,12 @@ var _ = require('lodash');
 */
 exports.initLocals = function (req, res, next) {
     res.locals.navLinks = [
-        { label: 'Home', key: 'home', href: '/' },
+        { label: 'About Me', key: 'about', href: '/about' },
+        { label: 'Projects', key: 'projects', href: '/projects' },
+        { label: 'Articles', key: 'articles', href: '/articles' },
         { label: 'Contact', key: 'contact', href: '/contact' }
     ];
+
     res.locals.user = req.user;
     next();
 };
